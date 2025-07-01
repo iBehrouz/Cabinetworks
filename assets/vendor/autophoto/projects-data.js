776ff49a76e92ccbdd644b736ef52c3f32a108da
iBehrouz <behr0x@gmail.com>
2025-07-02 00:13:25 +1000

    k

---
+++ assets/vendor/autophoto/projects-data.js
@@ -0,0 +1,15 @@
+const projects = [
+  {
+    src: "assets/img/projects/shaker-1.jpg",
+    title: "Shaker Style",
+    desc: "Classic and clean",
+    filter: "filter-shaker"
+  },
+  {
+    src: "assets/img/projects/handleless-1.jpg",
+    title: "Handleless",
+    desc: "Minimal and modern",
+    filter: "filter-handleless"
+  },
+  // Add more entries here...
+];
